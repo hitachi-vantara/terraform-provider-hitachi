44f3344a1e7b7d77169a2cffc43840d7f0670f8d
rsahu <rabi.sahu@hitachivantara.com>
2023-08-20 21:36:58 -0700

    removed test dir

--- test/data-sources/hitachi_vsp_hostgroup/hostgroup_all.tf
+++
@@ -1,8 +0,0 @@
-data "hitachi_vsp_hostgroups" "hostgroups" {
-  serial   = 40014
-  port_ids = ["CL7-C", "CL7-A", "CL8-B", "CL9-C"]
-}
-
-output "hostgroups" {
-  value = data.hitachi_vsp_hostgroups.hostgroups
-}
--- test/data-sources/hitachi_vss_block_storage_pools/data-source.tf
+++
@@ -1,8 +0,0 @@
-data "hitachi_vss_block_storage_pools" "pool" {
-  vssb_address = "10.76.47.55"
-  // storage_pool_names = ["SP01"]
-}
-
-output "pool" {
-  value = data.hitachi_vss_block_storage_pools.pool
-}
