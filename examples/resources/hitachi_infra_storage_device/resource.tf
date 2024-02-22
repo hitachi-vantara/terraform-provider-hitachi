6e7671980a4c6848ced7db72f5288b42f4db81ed
Rabinarayan Sahu <rabi.sahu@hitachivantara.com>
2024-02-22 17:07:45 +0530

    Triggered new build

--- examples/resources/hitachi_infra_storage_device/resource.tf
+++ examples/resources/hitachi_infra_storage_device/resource.tf
@@ -6,7 +6,6 @@ resource "hitachi_infra_storage_device" "demo_sd" {
   gateway_address = "172.25.20.35"
   #system = "UCP-CI-12035"
 
-
   // Increase the timeout value for create/update operations accordingly
     timeouts {
     create = "10m"
