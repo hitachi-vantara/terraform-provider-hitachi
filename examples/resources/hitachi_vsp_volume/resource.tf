a7c8e57f81777a2b51d2ec4af0b5f0b35e02201f
Rabinarayan Sahu <rabi.sahu@hitachivantara.com>
2024-03-05 10:43:31 +0530

    Added docs for volume resource

--- examples/resources/hitachi_vsp_volume/resource.tf
+++ examples/resources/hitachi_vsp_volume/resource.tf
@@ -9,6 +9,32 @@
 // desired volume configuration.
 //
 
+/* 
+Refre to the below table for more information
+
+| Property                      | Gateway Provider           | VSP Direct Connect Provider  |
+|-------------------------------|----------------------------|------------------------------|
+| serial                        | 12345 **                   | 12345 *                      |
+| storage_id                    | ""   **                    | -                            |
+| size_gb                       | 1  *                       | 1 *                          |
+| pool_id                       | 2  *                       | 1 **                         |
+| name                          | "SampleName"               | "SampleName"                 |
+| system                        | "SampleSystemName"         | -                            |
+| subscriber_id                 | ""                         | -                            |
+| resource_group_id             | ""                         | -                            |
+| ldev_id                       | 2                          | -                            |
+| deduplication_compression_mode| ""                         | -                            |
+| pool_name                     | -                          | "PoolName"   **              |
+| paritygroup_id                | -                          | "parity_group_id" **         |
+
+
+
+- Not supported
+** Either or one of the parameter required
+* Required parameters
+
+*/
+
 resource "hitachi_vsp_volume" "mylun" {
   serial  = 12345
   size_gb = 1
