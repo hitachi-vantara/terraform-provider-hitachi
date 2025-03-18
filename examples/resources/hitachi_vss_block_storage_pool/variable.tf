2a57e642aa0d21c0489e52950c1c6b76725345e2
Humphrey Alba <humphrey.alba@hitachivantara.com>
2025-03-18 16:05:48 -0400

    terraform examples, test,docs

---
+++ examples/resources/hitachi_vss_block_storage_pool/variable.tf
@@ -0,0 +1,16 @@
+variable "hitachi_storage_user" {
+  type        = string
+  description = "Username of the Hitachi storage system."
+  sensitive   = true
+}
+
+variable "hitachi_storage_password" {
+  type        = string
+  description = "Password of the Hitachi storage system."
+  sensitive   = true
+}
+
+variable "vssb_address" {
+  description = "VSSB block address"
+  type        = string
+}
--- test/data-sources/hitachi_vsp_dynamic_pools/clean.sh
+++ test/data-sources/hitachi_vsp_dynamic_pools/clean.sh
@@ -1,4 +1,4 @@
-!#/bin/sh
+#!/bin/sh
 
 echo "Start cleaning terraform files"
 rm -rf .terraform
