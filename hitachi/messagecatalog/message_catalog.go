2586afe626af49ff944d99c50e4a2ea18dd2150b
Humphrey Alba <humphrey.alba@hitachivantara.com>
2025-06-24 13:56:24 -0400

    remove unused files, build is ok

--- hitachi/messagecatalog/message_catalog.go
+++
@@ -1,7 +0,0 @@
-package messagecatalog
-
-// for localization, one option is to replace this file with localized resource
-var MessageCatalog = map[interface{}]string{
-	Default1:      "%v",
-	ERR_GET_LUN_1: "Error getting the lun information from storages %d",
-}
